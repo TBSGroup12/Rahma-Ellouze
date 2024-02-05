ce88b96040f5edd9b0ab603734e31d83ad14d2b9
rahmaellouze <rahmaellouze.tbs@gmail.com>
2024-02-05 03:39:42 -1200

    w2 my first react component

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,69 @@
+
+
+
+
+
+/* import * as React from "REACT";
+const welcome={
+  greetings:"hey",
+  title:"react",
+};
+function App (){
+  return(
+  <div>
+    <h1>
+      {welcome.greetings} {welcome.title}
+    </h1>
+    <label htmlFor="search">search:</label>
+    <input id="search" type="text"/>
+  </div>
+  );
+}
+export default App; */
+/* import * as REACT from "react";
+function App() {
+  const title="React";
+  return(
+    <div>
+      <h1>Hello {title}</h1>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+export default App; */
+
+/*import * as REACT from "react";
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
+function App() {
+  return(
+    <div>
+      <h1> {welcome.greeting} {welcome.title} </h1>
+      <label htmlFor="search">search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+
+} 
+export default App;
+*/
+
+/*import * as REACT from "react";
+const welcome = {
+  greeting: "Hey",
+  title: "React",
+};
+function App() {
+  return(
+    <div>
+      <h1> {welcome.greeting} {welcome.title} </h1>
+      <label htmlFor="search">search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+export default App;*/
+
