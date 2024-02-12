d19eafaad757ca28cf2348e1061f5583a288cc59
rahmaellouze <rahmaellouze.tbs@gmail.com>
2024-02-12 02:51:34 -1200

    w3 lists in read

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -67,3 +67,19 @@ function App() {
 }
 export default App;*/
 
+const list =[
+  {title:"React",url:"https//reactjs.org/",author:"jordan walke",num_comments:3,points:4,objectID:0,},
+  {title:"Redux",url:"https//redux.js.org/",author:"dan abramov, andrew clark",num_comments:2,points:5,objectID:1,},
+];
+function App(){
+  return(
+    <div>
+      <h1>MY Hacker Stories</h1>
+      <label htmlFor="search" type="text"/>
+      <hr />
+      <ul>
+        {list.map(function(item) {return <li>{item.title}</li>})}
+      </ul>
+    </div>
+  );
+}
